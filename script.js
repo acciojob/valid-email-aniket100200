b9d01960a25ca5e2cdc1c8aa45083438ff266863
Aniket Khangar <116418804+aniket100200@users.noreply.github.com>
2023-12-20 09:09:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
 function validEmail(str) {
   //your JS code here.
+	if(!str)return false;
+
+	let arr=str.split(".");
+
+	let rate=arr[0];
+	let sRate=arr[0].split("@");
+
+	if(sRate.length!=2)return false;
+	if(arr[0].charAt(0)==='@')return false;
+	if(arr[0].charAt(arr[0].length-1)==='@')return false;
+
+	//case .se end ho rha tb bhi..
+	if(arr[arr.length-1].length===0)return false;
+
+	return true;
+	
 }
 
 // Do not change the code below.
 const str = prompt("Enter an email address.");
+
 alert(validEmail(str));
